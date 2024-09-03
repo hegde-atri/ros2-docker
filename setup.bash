a1dbdae1ffbb9668df96419a3ff985f5260d96ca
hegde-atri <iamatrihegde@outlook.com>
2024-09-03 21:54:32 +0100

    fix: simplified ros_dev + README

--- setup.bash
+++ setup.bash
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 ros_dev() {
-  if (( $# % 2 != 0 )); then
-    echo "Usage: ros_dev <container_name1> <project_path1> [<container_name2> <project_path2> ...]"
+  if (( $# != 2 )); then
+    echo "Usage: ros_dev <container_name1> <project_path1>"
     return 1
   fi
 
-  while (( $# >= 2 )); do
-    ROS_DEV_CONTAINER_NAME=$1
-    ROS_PROJECT_PATH=$2
-    shift 2
+  ROS_DEV_CONTAINER_NAME=$1
+  ROS_PROJECT_PATH=$2
 
-    cd "$ROS2_DOCKER_DIR" && docker-compose up -d --build
-  done
+  cd $ROS2_DOCKER_DIR && docker compose up -d --build
 }
 
 ros_shell() {
