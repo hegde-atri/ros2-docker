012ef4d00e08246ffc009c5f3cd4515fa7b9d994
Atri Hegde <iamatrihegde@outlook.com>
2024-09-03 21:59:09 +0100

    feat: simplified macOS setup

--- setup.bash
+++ setup.bash
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 
 ros_dev() {
-  if (( $# != 2 )); then
-    echo "Usage: ros_dev <container_name1> <project_path1>"
-    return 1
-  fi
-
-  ROS_DEV_CONTAINER_NAME=$1
-  ROS_PROJECT_PATH=$2
+  ROS_PROJECT_PATH=$(pwd)
 
   cd $ROS2_DOCKER_DIR && docker compose up -d --build
 }
 
 ros_shell() {
-    if (( $# != 1 )); then
-        echo "Usuage: ros_shell <container_name>"
-        return 1
-    fi
-
-    docker exec -it $1 /bin/zsh
+    docker exec -it ros2 /bin/zsh
 }
