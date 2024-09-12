ceab833fa9f7a7e067b6fa0bb9677316aa341bef
Atri Hegde <iamatrihegde@outlook.com>
2024-09-12 14:08:14 +0000

    fix: windows volume and docker compose commands

--- windows.ps1
+++ windows.ps1
@@ -3,12 +3,13 @@ function Set-ROS2 {
     $env:VOLUME_MAPPING = "${location}:/ros2_ws"
     $env:ROS2_DOCKER_ENV = "C:\Users\iamat\Documents\repos\ros2-docker"
     $env:ROS2_DOCKER_COMPOSE_START_COMMAND = "docker compose -f docker-compose.windows.yml up -d --build"
-    $env:ROS2_DOCKER_COMPOSE_STOP_COMMAND = "docker compose -f docker-compose.windows.yml down"
-    $env:ROS2_DOCKER_SHELL_COMMAND = "docker exec -it ros2"
+    $env:ROS2_DOCKER_COMPOSE_STOP_COMMAND = "docker compose down"
+    $env:ROS2_DOCKER_SHELL_COMMAND = "docker exec -it ros2 /bin/zsh"
 
     Write-Host "ROS2 development env-variables are set:"
     Write-Host "VOLUME_MAPPING: $env:VOLUME_MAPPING"
-    Write-Host "ROS2_DOCKER_COMPOSE_COMMAND: $env:ROS2_DOCKER_COMPOSE_COMMAND"
+    Write-Host "ROS2_DOCKER_COMPOSE_START_COMMAND: $env:ROS2_DOCKER_COMPOSE_START_COMMAND"
+    Write-Host "ROS2_DOCKER_COMPOSE_STOP_COMMAND: $env:ROS2_DOCKER_COMPOSE_STOP_COMMAND"
 }
 
 function Start-ROS2 {
@@ -17,15 +18,17 @@ function Start-ROS2 {
     Invoke-Expression $env:ROS2_DOCKER_COMPOSE_START_COMMAND
     Pop-Location
     Write-Host "ROS2 development environment started."
-    Write-Host "Use the command Shell-ROS2 to enter it."
+    Write-Host "Use the command Enter-ROS2 to enter it."
 }
 
 function Enter-ROS2 {
+    Set-ROS2
     Write-Host "Creating ROS2 Shell"
-    Invoke-Expression $env.$env:ROS2_DOCKER_SHELL_COMMAND
+    Invoke-Expression $env:ROS2_DOCKER_SHELL_COMMAND
 }
 
 function Stop-ROS2 {
+    Set-ROS2
     Write-Host "Shutting down ROS2 container"
     Push-Location $env:ROS2_DOCKER_ENV
     Invoke-Expression $env:ROS2_DOCKER_COMPOSE_STOP_COMMAND
